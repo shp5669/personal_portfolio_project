0bac04fc861bce121150965e134e19ce71b4208b
Sean-Park <shp5669@gmail.com>
2024-07-20 13:11:36 +1000

    Edited superuser

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7fea)agzt#$+opj(@%htikxso#%(b-6os8n56a9uw*o9&yjm75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.nickwalter.info']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
